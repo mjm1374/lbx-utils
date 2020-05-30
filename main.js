ca97923e11a6b3cb9d83a1692407f979e1a09b9c
Mike McAllister <mike@logikbox.com>
2020-05-30 14:46:43 -0400

    added buiildmap function

--- main.js
+++ main.js
@@ -1,101 +1,113 @@
 // ESM syntax is supported.
 /**
- * adds or removes a class on a Element by className
+ * Adds or removes a class on a Element by className
  * @param {string} el the Element ID
- * @param {string} cls the class name to toggle 
+ * @param {string} cls the class name to toggle
  */
 export function toggleClass(el, cls) {
-    try {
-        if (el) {
-            let target = document.getElementById(el);
-            if (cls) {
-                if (target.classList.contains(cls)) {
-                    target.classList.remove(cls);
-                } else {
-                    target.classList.add(cls);
-                }
-            } else {
-                throw new Error("No Class Defined");
-            }
-        } else {
-            throw new Error("No Element Defined");
-        }
-    } catch (e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
+	try {
+		if (el) {
+			let target = document.getElementById(el);
+			if (cls) {
+				if (target.classList.contains(cls)) {
+					target.classList.remove(cls);
+				} else {
+					target.classList.add(cls);
+				}
+			} else {
+				throw new Error('No Class Defined');
+			}
+		} else {
+			throw new Error('No Element Defined');
+		}
+	} catch (e) {
+		console.log(`${e.name}: ${e.message}`);
+	}
 }
 
 /**
- * function for setting the DISPLAY property to block or hide, 
- * @param {string} el the Element ID 
- * @param {boolean} visible the value to set 
+ * Function for setting the DISPLAY property to block or hide,
+ * @param {string} el the Element ID
+ * @param {boolean} visible the value to set
  */
 export function setVisible(el, visible) {
-    try {
-        if (el) {
-            let target = document.getElementById(el);
-            if (visible == true) {
-                target.style.display = "block";
-            } else {
-                target.style.display = "none";
-            }
-        } else {
-            throw new Error("No Element Defined");
-        }
-    } catch (e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
+	try {
+		if (el) {
+			let target = document.getElementById(el);
+			if (visible == true) {
+				target.style.display = 'block';
+			} else {
+				target.style.display = 'none';
+			}
+		} else {
+			throw new Error('No Element Defined');
+		}
+	} catch (e) {
+		console.log(`${e.name}: ${e.message}`);
+	}
 }
 
 /**
- * shortcut for setVisible Function to show an element, 
- * @param {string} el the Element ID 
+ * Shortcut for setVisible Function to show an element,
+ * @param {string} el the Element ID
  */
 export function show(el) {
-    try {
-        if (el) {
-            setVisible(el, true)
-        } else {
-            throw new Error("No Element Defined");
-        }
-    } catch (e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
+	try {
+		if (el) {
+			setVisible(el, true);
+		} else {
+			throw new Error('No Element Defined');
+		}
+	} catch (e) {
+		console.log(`${e.name}: ${e.message}`);
+	}
 }
 
 /**
- * shortcut for setVisible Function to hide an element, 
+ * Shortcut for setVisible Function to hide an element,
  * @param {string} el the Element ID
  */
 export function hide(el) {
-    try {
-        if (el) {
-            setVisible(el, false)
-        } else {
-            throw new Error("No Element Defined");
-        }
-    } catch (e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
+	try {
+		if (el) {
+			setVisible(el, false);
+		} else {
+			throw new Error('No Element Defined');
+		}
+	} catch (e) {
+		console.log(`${e.name}: ${e.message}`);
+	}
 }
 
 /**
- * check and toggle an elements visability, 
- * @param {string} el the Element ID {string}
+ * Check and toggle an elements visability,
+ * @param {string} el the Element ID
  */
 export function toggleDisplay(el) {
-    try {
-        if (el) {
-            let target = document.getElementById(el);
-            if (target.style.display != "none") {
-                setVisible(el, false);
-            } else {
-                setVisible(el, true)
-            }
-        } else {
-            throw new Error("No Element Defined");
-        }
-    } catch (e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
+	try {
+		if (el) {
+			let target = document.getElementById(el);
+			if (target.style.display != 'none') {
+				setVisible(el, false);
+			} else {
+				setVisible(el, true);
+			}
+		} else {
+			throw new Error('No Element Defined');
+		}
+	} catch (e) {
+		console.log(`${e.name}: ${e.message}`);
+	}
+}
+
+/**
+ * Convert an Object to a Map
+ * @param {string} obj the object to convert to a map
+ */
+export function buildMap(obj) {
+	let map = new Map();
+	Object.keys(obj).forEach((key) => {
+		map.set(key, obj[key]);
+	});
+	return map;
 }
